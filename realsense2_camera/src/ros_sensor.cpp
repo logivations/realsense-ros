2df735d3a26d4af3e74790fce79138a9b870bc6c
Vladyslav Hrynchak <vladyslav.hrynchak@logivations.com>
2024-11-29 09:55:45 +0200

    fix RosSensor constructor

--- realsense2_camera/src/ros_sensor.cpp
+++ realsense2_camera/src/ros_sensor.cpp
@@ -55,7 +55,7 @@ RosSensor::RosSensor(rs2::sensor sensor,
     _hardware_reset_func(hardware_reset_func),
     _diagnostics_updater(diagnostics_updater),
     _force_image_default_qos(force_image_default_qos),
-    _min_fps_threshold(parameters.getParameters()->readAndDeleteParam("min_fps_threshold", 10.0))
+    _min_fps_threshold(parameters.getParameters()->getParam<double>("min_fps_threshold"))
 {
     ROS_ERROR_STREAM("RosSensor started");
 
