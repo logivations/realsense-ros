2e6d41287f99b1487802f186ff90c4a12e2d4030
Vladyslav Hrynchak <vladyslav.hrynchak@logivations.com>
2024-12-10 12:52:02 +0200

    update copy constructor

--- realsense2_camera/include/ros_sensor.h
+++ realsense2_camera/include/ros_sensor.h
@@ -46,7 +46,7 @@ namespace realsense2_camera
             _name(other._name),
             _min_freq(other._min_freq),
             _max_freq(other._max_freq),
-            _freq_status_param(&_min_freq, &_max_freq, 0.1, 10),
+            _freq_status_param(&_min_freq, &_max_freq, 0.0, 10),
             _freq_status(_freq_status_param, _name),
             _p_updater(other._p_updater)
             {
